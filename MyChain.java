71619888549a48b86a1747c68c44cfc92b221093
Prakhar Pandey <92363409+Prakhar301101@users.noreply.github.com>
2024-07-05 20:30:42 +0530

    Add abstraction & Improve Code

--- MyChain.java
+++ MyChain.java
@@ -5,14 +5,46 @@ import com.google.gson.GsonBuilder;
 public class MyChain {
     public static ArrayList<Block> blockchain=new ArrayList<Block>();
     public static void main(String[] args) {
-        blockchain.add(new Block("The first Block", "0"));
-        blockchain.add(new Block("The second Block",blockchain.get(blockchain.size()-1).hash));
-        blockchain.add(new Block("The third Block", blockchain.get(blockchain.size()-1).hash));
-        blockchain.add(new Block("The fourth Block", blockchain.get(blockchain.size()-1).hash));
-        blockchain.add(new Block("The fifth Block", blockchain.get(blockchain.size()-1).hash));
-
-        String blockchainJson=new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
-        System.out.println(blockchainJson);
+        createBlock("The first block");
+        createBlock("The second block");
+        createBlock("The third block");
+        createBlock("The fourth block");
+        createBlock("The fifth block");
+        view_Chain();        
 
     }
+
+    //isValid will check for the integrity of the blockchain
+    //It will compare the hash of the block with the calculated hash value
+    //also the hash value of the previous block with prev_Hash value 
+    public static void createBlock(String data){
+        blockchain.add(new Block(data,getPrevHash()));
+    }
+    public static String getPrevHash(){
+        return blockchain.size()==0?"0":blockchain.get(blockchain.size()-1).hash;
+    }
+    public static void view_Chain(){
+        String blockchainJson=new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);
+    }
+    public static boolean isValid(){
+        Block curBlock;
+        Block prevBlock;
+        for(int i=1;i<blockchain.size();i++){
+            curBlock=blockchain.get(i);
+            prevBlock=blockchain.get(i-1);
+            //compare hash of cur block with calculated hash of cur block
+            if(!curBlock.hash.equals(curBlock.calculateHash())){
+                return false;
+            }
+            //compare hash of prev block with the registered hash of prev block stored in cur block
+            if(!prevBlock.hash.equals(curBlock.prev_Hash)){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
+
+//To Stop tampering of data we use proof of work
--- README.md
+++ README.md
@@ -8,4 +8,18 @@ A block contains 2 Hash values its own and the previous block's, the hash value 
 </p>
 
 
+<h3>How to create Blocks on MyChain</h3>
+<p>
+Just use the <span><b>createBlock</b></span> function, send your string data as parameter and call the createBlock function.
+The createBlock function creates a Block object by using the data, the previous block's hash and the calculated hash value of the current block.
+</p>
 
+<h3>To view the Blockchain</h3>
+<p>Use the <span><b>view_Chain</b></span> function to get the information of the blockchain in JSON format.
+A library called gson is used to convert the output string into JSON data.
+</p>
+
+<h3>To check integrity of the Blockchain</h3>
+<p>Use the <span><b>isValid</b></span> function.
+It works by comparing the hash values between the current and previous blocks 
+ </p>
